b6e786a295e3573f7f7ceece48bac76adc53ef1f
arm64be <mel@x86.company>
2026-04-23 17:02:00 +0200

    fix formatting

--- theia-panel/src/ui/utils.ts
+++ theia-panel/src/ui/utils.ts
@@ -1,7 +1,10 @@
 export function escape(s: string): string {
   return s.replace(
     /[&<>"']/g,
-    (c) => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" })[c]!,
+    (c) =>
+      ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" })[
+        c
+      ]!,
   );
 }
 
